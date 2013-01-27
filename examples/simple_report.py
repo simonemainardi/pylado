dfbe6194ecd184300b9e7f4baa6e43c44f4def67
Simone Mainardi <simone.mainardi@iet.unipi.it>
2013-01-27 19:58:40 +0100

    added an example folder with a simple example

---
+++ examples/simple_report.py
@@ -0,0 +1,24 @@
+import os,sys
+import random
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir)
+
+from structure import structure as s
+from document import document
+
+# instantiate a new document, specifying its title and the name
+# of the author
+d = document(author="Simone Mainardi", title="Yet Another \LaTeX{} report")
+
+# generate some random content
+randomcontent = ''.join(random.choice(' asd.,') for i in xrange(1000))
+
+# create a section named "One section" containing the previously
+# generated random content
+d.append(s.section("One section", randomcontent))
+
+# create another section
+d.append(s.section("Another Section", randomcontent))
+
+# write the pdf file
+d.generate_pdf("output/simple_report.pdf")
