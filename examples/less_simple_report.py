0f1c58dab2d5efde29d22257f1d4fc0dbc8c337e
Simone Mainardi <simone.mainardi@iet.unipi.it>
2013-01-29 00:50:37 +0100

    added a new image file and a new, more detailed, report exampe

---
+++ examples/less_simple_report.py
@@ -0,0 +1,43 @@
+import os,sys
+import random
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir)
+
+from texifier import structure as s
+from document import document
+
+# instantiate a new document, specifying its title and the name
+# of the author
+d = document(author="Simone Mainardi", title="Yet Another \LaTeX{} not-so-simple Report")
+
+# generate some random content
+randomcontent = ''.join(random.choice(' asd.,') for i in xrange(1000))
+
+# create an abstract
+d.append(s.abstract(randomcontent))
+
+# add the table of contents
+d.append(s.tableofcontents())
+
+# add the table of figures
+d.append(s.listoffigures())
+
+# create five sections, each one with and image and three subsections
+for sec in xrange(5):
+    # add the section ...
+    d.append(s.section("Senseless Section %i" % (sec + 1), randomcontent))
+
+    # ... the image
+    d.append(s.image("images/a_graph.eps",
+                     "This is the senseless graph of Section %i" % (sec+1),
+                     "width=.96\\textwidth"))
+
+    # and the subsections
+    for sub in xrange(3):
+        d.append(s.subsection("Senseless Subsection %i" % (sub+1), randomcontent))
+
+# write the pdf file
+# be careful that sometimes pdflatex requires to compile twice
+# prior to show up references and table of contents
+for i in xrange(2):
+    d.generate_pdf("output/less_simple_report.pdf")
--- texifier.py
+++ texifier.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 
 available_structures = ["section", "subsection"]
+available_summaries = ["tableofcontents", "listoffigures", "listoftables"]
 
 class structure(object):
     """
@@ -12,6 +13,8 @@ class structure(object):
     def __init__(self):
         # dynamically attach some methods generating
         # latex code for given structure names
+
+        # dynamic methods for structues such as sections and subsections...
         for struct in available_structures:
             def texify(self, structname, structcontents, numbered=False, struct=struct):
                 texcode = "\\" + struct + "*{" + structname + "}\n"
@@ -19,15 +22,63 @@ class structure(object):
                     texcode = texcode.replace("*", "")
                 texcode += structcontents
                 return texcode
-            texify.__doc__ = """ Generate tex code for a LaTeX %s.
+            texify.__doc__ = '''
+                Generate tex code for a LaTeX %s.
                 The %s will be named structname and will contain contents specified in 
                 structcontents. You may also specify whether you need a numbered
                 %s, but you should not override the last parameter to avoid unwanted
-                function behaviour.
-                """ % (struct, struct, struct)
+                funection behaviour.
+
+                The output will be, if numbered=False:
+                \%s{structname}
+                structcontents
+
+                Else, if numbered=True:
+                \%s*{structname}
+                structcontents
+                ''' % tuple([struct] * 5)
             texify.__name__ = struct
             # attach an attribute named struct pointing to the
             # function texify with the right default parameters
             setattr(self.__class__, struct, texify)
+
+        # dynamic  methods for summaries such as the table of contents
+        for summ in available_summaries:
+            def texify(self, summ=summ):
+                tex = "\\" + summ + "\n"
+                tex = tex.replace("\t","\\t")
+                return tex
+            texify.__doc__ = '''
+                Generate tex code for a LaTeX %s.
+
+                The output will be, if numbered=False:
+                \%s
+                ''' % tuple([summ] * 2)
+            texify.__name__ = summ
+            # dynamically attach texify method to the class
+            setattr(self.__class__, summ, texify)
+
+    def abstract(self, contents=""):
+        return "\\begin{abstract}\n%s\n\end{abstract}\n" % contents
+
+    def image(self, imgfile, caption=None, includegraphicsopts=""):
+        """
+        Generate LaTeX code for an image. The image
+        included in the code
+        """
+        # check if the file exists and can be opened
+        # otherwise raise an exception
+        file(imgfile, "r")
+        tex = "\\begin{figure}[t]\n\centering\n"
+
+        # include the graphics file and possibly its options
+        tex += "\includegraphics[%s]{%s}\n" % (includegraphicsopts, imgfile)
+        
+        # determine if we have to add a caption
+        if caption and isinstance(caption, basestring):
+            tex += "\caption{%s}\n" % caption
+
+        tex += "\end{figure}"
+        return tex
 
 structure = structure()
